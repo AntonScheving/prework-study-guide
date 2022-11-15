fe56a2aeb40125536509a1c5f35309467305c2b9
AntonScheving <115781123+AntonScheving@users.noreply.github.com>
2022-11-15 14:57:20 +0000

    adding code to JS

--- prework-study-guide-3/assets/script.js
+++ prework-study-guide-3/assets/script.js
@@ -1,5 +1,35 @@
-var topic = "HTML";
-/*
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+function listTopics() {
+    for (var x = 0; x < topics.length; x++) {
+      console.log(topics[x]);
+    }
+   }
+
+function selectTopic() {
+    if (randomTopic === 'HTML') {
+   console.log("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+   console.log("Let's study CSS!");
+    } else if (randomTopic === 'Git') {
+   console.log("Let's study Git!");
+    } else if (randomTopic === 'JavaScript') {
+   console.log("Let's study JavaScript!");
+     } else {
+   console.log('Please try again!');
+ }
+}
+console.log('Here are the topics we learned through Prework:');
+    listTopics();
+
+console.log('Which topic should we study first?');
+    selectTopic();
+
+
+/*var topic = "HTML"
+
 if (topic === 'HTML') {
     console.log("Let's study HTML!");
     } else if (topic === 'CSS') {
@@ -10,10 +40,29 @@ if (topic === 'HTML') {
     console.log("Let's study JavaScript");
     } else {
         console.log('Please try again!');
-    }*/
-   
+    }
 
     var topic = ["HTML", "CSS", "Git", "JavaScript"];
-    for(var x = 0; x < topic.length; x++) {
-     console.log(topic[x]);
+        for(var x = 0; x < topic.length; x++) {
+          console.log(topic[x]);
     }
+
+    function listTopics() {
+        for (var x = 0; x < topics.length; x++) {
+          console.log(topics[x]);
+        }
+      }
+
+      function selectTopic() {
+        if (topic === 'HTML') {
+          console.log("Let's study HTML!");
+        } else if (topic === 'CSS') {
+          console.log("Let's study CSS!");
+        } else if (topic === 'Git') {
+          console.log("Let's study Git!");
+        } else if (topic === 'JavaScript') {
+          console.log("Let's study JavaScript!");
+        } else {
+          console.log('Please try again!');
+        }
+      }*/
