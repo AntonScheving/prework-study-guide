72cd72338369b1c71370dd6891c0dbc7ac0eb91f
AntonScheving <115781123+AntonScheving@users.noreply.github.com>
2022-11-15 08:33:39 +0000

    Adding JacaScript code

---
+++ prework-study-guide-3/assets/script.js
@@ -0,0 +1,19 @@
+var topic = "HTML";
+/*
+if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+    } else if (topic === 'CSS') {
+    console.log("Let' study CSS!");
+    } else if (topic === 'Git') {
+    console.log("Let' study Git");
+    } else if (topic === 'JavaScript') {
+    console.log("Let's study JavaScript");
+    } else {
+        console.log('Please try again!');
+    }*/
+   
+
+    var topic = ["HTML", "CSS", "Git", "JavaScript"];
+    for(var x = 0; x < topic.length; x++) {
+     console.log(topic[x]);
+    }
